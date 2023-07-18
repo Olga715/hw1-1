46cbc6972055fce8afbc07ae9d0e0f01615a49eb
Olga <olyab.715@gmail.com>
2023-07-19 00:57:07 +0500

    Changed hw1-1

---
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FreelanceServiceTest {
+
+    @Test
+    public void testCalculate1() {
+        FreelanceService service = new FreelanceService();
+
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalculate2() {
+        FreelanceService service = new FreelanceService();
+
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
+
